b796c78f096614fd116cda94518376689d277213
grbe22 <113363608+grbe22@users.noreply.github.com>
2024-02-08 12:59:38 -0500

    Updated load processes
    It loads the map in .01 time now. So. I guess I was being really, REALLY, stupid.
    
    I have to figure out why it's using the wrong values, but I'm in class and will take care of that later :)

--- Scripts/MapGrid.cs
+++ Scripts/MapGrid.cs
@@ -1,124 +1,109 @@
 using Godot;
 using System;
+using System.Diagnostics;
+using System.Threading;
 
 public partial class MapGrid : Sprite2D
 {
-	private const int CELL_SIZE = 32;
-	int edgeSize = 150;
+	int edgeSize = 250;
 	int perlinScale = 7;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Perlin mapMaker = new Perlin(edgeSize, perlinScale);
+		var timer = new Stopwatch();
+		timer.Start();
 		// heightMap is for generating sea level and terrain level.
 		float[,] heightMap;
 		heightMap = mapMaker.PerlinGenerator(true);
+		GD.Print("HeightMap Complete at time ", timer.Elapsed);
 		// heatMap is for generating the temperature of the terrain - low temperature forms ice, high forms badlands.
 		float[,] heatMap;
 		mapMaker.UpdatePerlinMap(perlinScale / 2);
 		heatMap = mapMaker.PerlinGenerator(false);
+		GD.Print("HeatMap Complete at ", timer.Elapsed);
 		// moistureMap isn't really for moisture, but I dont know what a better name would be.
 		// High values generate forest & overgrown areas, low generate swamps and "murky" areas.
 		float[,] moistureMap;
 		mapMaker.UpdatePerlinMap(perlinScale * 4);
 		moistureMap = mapMaker.PerlinGenerator(false);
+		GD.Print("MoistureMap Complete at ", timer.Elapsed);
 		
 		// generates the map using these three maps.
-		GenerateGrid(heightMap, heatMap, moistureMap);
+		GenerateGrid(heightMap, heatMap, moistureMap, timer);
+		GD.Print("Texture loaded at ", timer.Elapsed);
 		Position = new Vector2(0, 0);
 	}
 
-	private void GenerateGrid(float[,] heightMap, float[,] heatMap, float[,] moistureMap) {
-		Image image = Image.Create(edgeSize * CELL_SIZE, edgeSize * CELL_SIZE, false, Image.Format.Rgba8);
-		// textures for each terrain type.
-		Image oceanTexture = GD.Load<Image>("res://Assets/Tiles/ocean.png");
-		Image groundTexture = GD.Load<Image>("res://Assets/Tiles/ground.png");
-		Image beachTexture = GD.Load<Image> ("res://Assets/Tiles/beach.png");
-		Image mountainTexture = GD.Load<Image> ("res://Assets/Tiles/mountain.png");
-		Image iceTexture = GD.Load<Image> ("res://Assets/Tiles/ice.png");
-		Image tundraTexture = GD.Load<Image> ("res://Assets/Tiles/tundra.png");
-		Image badlandsTexture = GD.Load<Image> ("res://Assets/Tiles/badlands.png");
-		Image forestTexture = GD.Load<Image> ("res://Assets/Tiles/forest.png");
-		Image swampTexture = GD.Load<Image> ("res://Assets/Tiles/swamp.png");
-
-		// Loop through the grid data and draw cells
+	private void GenerateGrid(float[,] heightMap, float[,] heatMap, float[,] moistureMap, Stopwatch timer) {
+		// Loop through the grid data and paste cells
+		// loads the TileMap
+		TileMap foundation = (TileMap)GetChild(0);
 		for (int y = 0; y < edgeSize; y++) {
+			if (y % 25 == 0) {
+				GD.Print("Y-Value ", y, " was completed at ", timer.Elapsed);
+			}
 			for (int x = 0; x < edgeSize; x++) {
+				// creates the 
+				int tileId;
 				
 				// Grab the info for the tile from the three arrays
-				
 				float heightValue = heightMap[y, x];
 				float heatValue = heatMap[y, x];
 				float moistureValue = moistureMap[y, x];
-
-                Image imagery;
-                
 				// Choose texture based on each array
-                if (heightValue < .5) {
+				if (heightValue < .5) {
 
 					// generates ocean, or icebergs in very cold climates.
-					if (heatValue < .16) { imagery = iceTexture; }
+					if (heatValue < .16) { tileId = 2; }
 
-					else { imagery = oceanTexture; }
-
+					else { tileId = 1; }
 				} else if (heightValue < .55) {
 
 					// A small window that generates coastline. In exceedingly cold areas, generates tundra, and generates
 					// badlands in very high temperatures.
-					if (heatValue < .20) { imagery = tundraTexture; } 
+					if (heatValue < .20) { tileId = 4; } 
 
-					else if (heatValue > .80) { imagery = badlandsTexture; } 
+					else if (heatValue > .80) { tileId = 3; } 
 
-					else { imagery = beachTexture; }
+					else { tileId = 5; }
 				} else if (heightValue < .92 || moistureValue > .7) {
 					
 					// generates extreme temperatures in higher values. Badlands are moderately hot, tundra is moderately cold.
-					if (heatValue < .2) { imagery = tundraTexture; }
+					if (heatValue < .2) { tileId = 4; }
 					
-					else if (heatValue > .8) { imagery = badlandsTexture; } 
+					else if (heatValue > .8) { tileId = 3; } 
 
 					else {
 
 						// this is the default case which covers ~.6 of the tileSpan - it's distributed Gaussian-ly, so it's
 						// much more than just 60% of the tiles between .55 and .92.
 						// generates swamp in low temperatures, and forest in high temperatures.
-						if (moistureValue < .35) { imagery = swampTexture; } 
+						if (moistureValue < .35) { tileId = 6; } 
 
-						else if (moistureValue < .65) { imagery = groundTexture; } 
+						else if (moistureValue < .65) { tileId = 0; } 
 						
-						else { imagery = forestTexture;	}
+						else { tileId = 7; }
 					}
 				} else {
 					
 					// in VERY high temperatures, generates badlands, otherwise, generates mountains.
-					if (heatValue > .85) { imagery = badlandsTexture; } 
+					if (heatValue > .85) { tileId = 3; } 
 					
 					// mountains are very round blobs. I want SCRATCHES, like long canals of mountain, but I don't
 					// know how to go about this right now.
-					else { imagery = mountainTexture; }
+					else { tileId = 8; }
 				}
-				
 				// Draw the texture onto the image
-				DrawTextureOnImage(image, new Vector2(x * CELL_SIZE, y * CELL_SIZE), imagery);
+				PasteTexture(new Vector2I(2 * x - edgeSize, 2 * y - edgeSize), tileId, foundation);
 			}
 		}
-		Texture tex = new ImageTexture();
-		tex = ImageTexture.CreateFromImage(image);
-		Texture = (Texture2D)tex;
+		GD.Print(foundation.GetCellAtlasCoords(0, new Vector2I(0, 0)));
 	}
 	
-	private void DrawTextureOnImage(Image image, Vector2 position, Image texture) {
-		// Get the texture's data
-
-		// Loop through the texture's pixels and copy them to the image
-		for (int y = 0; y < texture.GetHeight(); y++)
-		{
-			for (int x = 0; x < texture.GetWidth(); x++)
-			{
-				Color color = texture.GetPixel(x, y);
-				image.SetPixel((int)(position.X + x), (int)(position.Y + y), color);
-			}
-		}
+	private void PasteTexture(Vector2I pos, int tileId, TileMap foundation) {
+		Vector2I atlasLoc = new Vector2I((int)(tileId / 8), tileId % 8);
+		foundation.SetCell(0, pos, 0, atlasLoc, 0);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
