2a2a621f48a01ffee26efbf6cfcf5fc479baf484
grbe22 <113363608+grbe22@users.noreply.github.com>
2024-02-08 10:23:51 -0500

    Formatted code in MapGrid.CS
    Also attempted to break up mountain ranges, but I haven't tested it.

--- Scripts/MapGrid.cs
+++ Scripts/MapGrid.cs
@@ -10,22 +10,27 @@ public partial class MapGrid : Sprite2D
 	public override void _Ready()
 	{
 		Perlin mapMaker = new Perlin(edgeSize, perlinScale);
+		// heightMap is for generating sea level and terrain level.
 		float[,] heightMap;
 		heightMap = mapMaker.PerlinGenerator(true);
+		// heatMap is for generating the temperature of the terrain - low temperature forms ice, high forms badlands.
 		float[,] heatMap;
 		mapMaker.UpdatePerlinMap(perlinScale / 2);
 		heatMap = mapMaker.PerlinGenerator(false);
+		// moistureMap isn't really for moisture, but I dont know what a better name would be.
+		// High values generate forest & overgrown areas, low generate swamps and "murky" areas.
 		float[,] moistureMap;
 		mapMaker.UpdatePerlinMap(perlinScale * 4);
 		moistureMap = mapMaker.PerlinGenerator(false);
 		
+		// generates the map using these three maps.
 		GenerateGrid(heightMap, heatMap, moistureMap);
 		Position = new Vector2(0, 0);
 	}
 
 	private void GenerateGrid(float[,] heightMap, float[,] heatMap, float[,] moistureMap) {
 		Image image = Image.Create(edgeSize * CELL_SIZE, edgeSize * CELL_SIZE, false, Image.Format.Rgba8);
-		// textures for later
+		// textures for each terrain type.
 		Image oceanTexture = GD.Load<Image>("res://Assets/Tiles/ocean.png");
 		Image groundTexture = GD.Load<Image>("res://Assets/Tiles/ground.png");
 		Image beachTexture = GD.Load<Image> ("res://Assets/Tiles/beach.png");
@@ -39,45 +44,64 @@ public partial class MapGrid : Sprite2D
 		// Loop through the grid data and draw cells
 		for (int y = 0; y < edgeSize; y++) {
 			for (int x = 0; x < edgeSize; x++) {
-				// Calculate the grayscale value
+				
+				// Grab the info for the tile from the three arrays
+				
 				float heightValue = heightMap[y, x];
 				float heatValue = heatMap[y, x];
 				float moistureValue = moistureMap[y, x];
-				// Choose texture based on grayscale value
-				Image imagery;
-				if (heightValue < .5) {
-					if (heatValue < .23) {
-						imagery = iceTexture;
-					} else {
-						imagery = oceanTexture;
-					}
+
+                Image imagery;
+                
+				// Choose texture based on each array
+                if (heightValue < .5) {
+
+					// generates ocean, or icebergs in very cold climates.
+					if (heatValue < .16) { imagery = iceTexture; }
+
+					else { imagery = oceanTexture; }
+
 				} else if (heightValue < .55) {
-					if (heatValue < .2) {
-						imagery = tundraTexture;
-					} else {
-						imagery = beachTexture;
-					}
+
+					// A small window that generates coastline. In exceedingly cold areas, generates tundra, and generates
+					// badlands in very high temperatures.
+					if (heatValue < .20) { imagery = tundraTexture; } 
+
+					else if (heatValue > .80) { imagery = badlandsTexture; } 
+
+					else { imagery = beachTexture; }
 				} else if (heightValue < .92) {
-					if (heatValue < .25) {
-						imagery = tundraTexture;
-					} else if (heatValue > .75){
-						imagery = badlandsTexture;
-					} else {
-						if (moistureValue < .35) {
-							imagery = swampTexture;
-						} else if (moistureValue < .65) {
-							imagery = groundTexture;
-						} else {
-							imagery = forestTexture;	
-						}
+					
+					// generates extreme temperatures in higher values. Badlands are moderately hot, tundra is moderately cold.
+					if (heatValue < .2) { imagery = tundraTexture; }
+					
+					else if (heatValue > .8) { imagery = badlandsTexture; } 
+
+					else {
+
+						// this is the default case which covers ~.6 of the tileSpan - it's distributed Gaussian-ly, so it's
+						// much more than just 60% of the tiles between .55 and .92.
+						// generates swamp in low temperatures, and forest in high temperatures.
+						if (moistureValue < .35) { imagery = swampTexture; } 
+
+						else if (moistureValue < .65) { imagery = groundTexture; } 
+						
+						else { imagery = forestTexture;	}
 					}
 				} else {
-					if (heatValue > .85) {
-						imagery = badlandsTexture;
-					} else {
-						imagery = mountainTexture;
+					
+					// in VERY high temperatures, generates badlands, otherwise, generates mountains.
+					if (heatValue > .85) { imagery = badlandsTexture; } 
+					
+					else {
+
+						// in wet areas, generates forests. I want to "break up" mountains, turn them from
+						// round blobs into LONG blobs. This isn't how I should do it.
+						if (moistureValue > .7) { imagery = forestTexture; }
+						else { imagery = mountainTexture; }
 					}
 				}
+				
 				// Draw the texture onto the image
 				DrawTextureOnImage(image, new Vector2(x * CELL_SIZE, y * CELL_SIZE), imagery);
 			}
